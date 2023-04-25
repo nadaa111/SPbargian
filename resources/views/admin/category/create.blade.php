68e012cd1ed9fba19c3cb43b7dab0d6dfec36af0
dalia <Daliazaitoun1@gmail.com>
2023-04-25 20:02:53 +0200

    add2

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -18,11 +18,14 @@
           <div class="col-md-6 mb-3">
             <label>Name</label>
             <input type="text" name="name" class="form-control"/>
+            @error('name') <small class ="text-danger">{{$message}}</small> @enderror
+
           </div>
 
           <div class="col-md-6 mb-3">
             <label>Slug</label>
             <input type="text" name="slug" class="form-control"/>
+            @error('slug') <small class ="text-danger">{{$message}}</small> @enderror
           </div>
           <div class="col-md-12 mb-3">
             <label>Description</label>
