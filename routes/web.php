687846ec4364c1f233aec1886d43dfd5b670ee96
dalia <Daliazaitoun1@gmail.com>
2023-04-25 16:37:01 +0200

    add0

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,9 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
 Route ::get("dashboard",[App\Http\Controllers\Admin\DashboardController::class, 'index']);
 
+//Category Routes
+Route ::get('category',[App\Http\Controllers\Admin\CategoryController::class, 'index']);
+Route ::get('category/create',[App\Http\Controllers\Admin\CategoryController::class, 'create']);
+Route ::post('category', [App\Http\Controllers\Admin\CategoryController::class, 'store']);
+
 });
