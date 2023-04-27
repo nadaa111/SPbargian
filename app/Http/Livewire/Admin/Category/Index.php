dada4b23952431a3b46b6399fa5c1ce5a41e328b
hanien361 <128161252+hanien361@users.noreply.github.com>
2023-04-27 03:00:31 +0200

    delete

--- app/Http/Livewire/Admin/Category/Index.php
+++ app/Http/Livewire/Admin/Category/Index.php
@@ -9,6 +9,15 @@ class Index extends Component
 {
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
+    public $category_id;
+
+public function deleteCategory($category_id)
+{
+dd($category_id);    
+$this->category_id = $category_id;
+}
+
+
     public function render()
     {
         $categories = Category::orderBy('id','DESC')->paginate(10);
--- resources/views/livewire/admin/category/index.blade.php
+++ resources/views/livewire/admin/category/index.blade.php
@@ -1,3 +1,34 @@
+<div> 
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">category delete</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form wire:submit.prevent="destroyCategory">
+      <div class="modal-body">
+        <h6>  Are you sure you want to delete this data? </h6>
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">yes. delete</button>
+      </div>
+</form>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
 <div class="row">
   <div class="col-md-12">
     @if(session('message'))
@@ -27,7 +58,7 @@
     <td>{{ $category->name  }} </td>
     <td>{{ $category->status == '1' ?'Hiddin' : 'Visible'  }} </td>
     <td> <a href=" {{url('admin/category/'.$category->id.'/edit') }} "class="btn btn-success">Edit </a>  </td>
-    <td> <a href=""class="btn btn-danger">Delete</a>  </td>
+    <td> <a href="#"  wire:click="deleteCategory({{  $category->id  }})"  data-bs-toggle="modal" data-bs-target="#deleteModal" class="btn btn-danger">Delete</a>  </td>
 </tr>
 @endforeach
 </tbody>
@@ -37,4 +68,5 @@
         </div>
     </div>
   </div>
+</div> 
 </div>
