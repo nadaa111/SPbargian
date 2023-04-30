d9229d4eb22953ff371010258b84013bcdd6196f
marimyako <marim.yacout@gmail.com>
2023-04-30 17:59:09 +0200

    home1

---
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Slider;
+
+class FrontendController extends Controller
+{
+     public function index ()
+     {
+        $sliders = Slider::where('status','0')->get();
+        return view('frontend.index',compact('sliders'));
+     }
+}
+ 
