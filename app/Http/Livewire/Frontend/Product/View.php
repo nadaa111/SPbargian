841be4b25aab0403de0b51bbbecbe615033c86e2
Eman-Ali55 <30202012353@alexu.edu.eg>
2023-05-06 16:56:20 +0200

    wish view

--- app/Http/Livewire/Frontend/Product/View.php
+++ app/Http/Livewire/Frontend/Product/View.php
@@ -3,11 +3,49 @@
 namespace App\Http\Livewire\Frontend\Product;
 
 use Livewire\Component;
+use App\Models\Wishlist;
+use Illuminate\Support\Facades\Auth;
 
 class View extends Component
-{
+{    public $category, $product;
+
+    public function addToWishList($productId)
+    {
+        if(Auth::check())
+        {
+          if(Wishlist::where('user_id',auth()->user()->id)->where('product_id',$productId)->exists())
+          {
+            session()->flash('message','Already added to wishlist');
+            return false;
+          }
+          else
+          {
+
+          
+          Wishlist::create([
+            'user_id' => auth()->user()->id,
+            'product_id' => $productId
+          ]);
+          session()->flash('message','Wishlist added successfully!');
+        }
+        }
+        else
+        {
+            session()->flash('message','Please login to continue');
+            return false;
+        }
+    }
+
+    public function mount($category, $product)
+    {
+        $this->category= $category;
+        $this->product= $product;
+    }
     public function render()
     {
-        return view('livewire.frontend.product.view');
+        return view('livewire.frontend.product.view',[
+            'category' => $this->category,
+            'product' => $this->product
+        ]);
     }
 }
