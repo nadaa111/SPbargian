adc00e5b24c4901ceae0bd8b2dec3ba9ad91d603
marimyako <marim.yacout@gmail.com>
2023-05-13 20:34:20 +0200

    user8

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,10 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+    public function userDetail()
+    {
+        return $this->hasOne(userDetail::class,'user_id','id');
+    }
     
 
     /**
