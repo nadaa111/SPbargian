fa5dca9599cc7e731968f561b011d30ae64ebf5a
marimyako <marim.yacout@gmail.com>
2023-05-13 19:51:51 +0200

    user4

--- app/Http/Controllers/Frontend/UserController.php
+++ app/Http/Controllers/Frontend/UserController.php
@@ -2,13 +2,43 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function index ()
     {
         return view ('trial.index');
+    }
+
+    public function updateUserDetails(Request $request)
+    {
+        $request->validate([
+            'username' => ['required','string'],
+            'phone' => ['required','digits:10'],
+            'pin_code' => ['required','digits:6'],
+            'address' => ['required','string','max:499'],
+
+
+        ]);
+
+        $user =User::findorFail(Auth::user()->id);
+        $user->update([
+            'name' => $request->username
+        ]);
+        $user->userDetail()->updateOrCreate(
+            [
+                'user_id' => $user->id,
+            ],
+            [
+                'phone' => $request->phone,
+                'pin_code' => $request->pin_code,
+                'address' => $request->address,
+            ]
+        );
+        return redirect()->back()->with('message','User Profile Updated');
     }
 }
