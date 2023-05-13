421f814e41a2f351e01055e213d4c811340041ca
marimyako <marim.yacout@gmail.com>
2023-05-13 19:51:59 +0200

    user5

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\userDetail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
-{
+{ 
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
@@ -22,6 +23,10 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+    public function  userDetail()
+    {
+         return  $this->hasOne(UserDetail::class,'user_id','id');
+    }
 
     /**
      * The attributes that should be hidden for serialization.
@@ -41,4 +46,5 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
 }
