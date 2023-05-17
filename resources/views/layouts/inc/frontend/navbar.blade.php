d4ceba2d78512c98edaf25ff22002ffa5b5507fd
marimyako <marim.yacout@gmail.com>
2023-05-17 20:01:02 +0200

    e store3

--- resources/views/layouts/inc/frontend/navbar.blade.php
+++ resources/views/layouts/inc/frontend/navbar.blade.php
@@ -4,7 +4,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-2 my-auto d-none d-sm-none d-md-block d-lg-block">
-                        <h5 class="brand-name">Funda Ecom</h5>
+                        <h5 class="brand-name">SP Bargian</h5>
                     </div>
                     <div class="col-md-5 my-auto">
                         <form role="search">
@@ -21,12 +21,12 @@
 
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ url('cart')}}">
-                                    <i class="fa fa-shopping-cart"></i> Cart (0)
+                                    <i class="fa fa-shopping-cart"></i> Cart 
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href=" {{ url('wishlist')}}">
-                                    <i class="fa fa-heart"></i> Wishlist (0)
+                                    <i class="fa fa-heart"></i> Wishlist 
                                 </a>
                             </li>
                                 <li class="nav-item">
@@ -62,10 +62,10 @@
 
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <i class="fa fa-user"></i>  {{ Auth::user()->name }}
+                                    <i class="fa fa-user"></i>  {{Auth::user()->name}}
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#"><i class="fa fa-user"></i> Profile</a></li>
+                                <li><a class="dropdown-item" href="{{ url('profile') }}"><i class="fa fa-user"></i> Profile</a></li>
                                 <li><a class="dropdown-item" href="#"><i class="fa fa-list"></i> My Orders</a></li>
                                 <li><a class="dropdown-item" href="#"><i class="fa fa-heart"></i> My Wishlist</a></li>
                                 <li><a class="dropdown-item" href="#"><i class="fa fa-shopping-cart"></i> My Cart</a></li>
@@ -96,7 +96,7 @@
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
                 <a class="navbar-brand d-block d-sm-block d-md-none d-lg-none" href="#">
-                    Funda Ecom
+                    E-store
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -124,9 +124,7 @@
                         <li class="nav-item">
                             <a class="nav-link" href="#">Accessories</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Home</a>
-                        </li>
+                       
                         <li class="nav-item">
                             <a class="nav-link" href="#">Appliances</a>
                         </li>
