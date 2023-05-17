b6f2d4ab9199c045535dc3a1a0768349e02ba74e
marimyako <marim.yacout@gmail.com>
2023-05-17 20:01:33 +0200

    e store4

--- resources/views/livewire/frontend/cart/cart-show.blade.php
+++ resources/views/livewire/frontend/cart/cart-show.blade.php
@@ -32,12 +32,12 @@
                                         <a href="">
                                             <label class="product-name">
                                                 <img src="hp-laptop.jpg" style="width: 50px; height: 50px" alt="">
-                                                Hp Laptop
+                                                Mobile
                                             </label>
                                         </a>
                                     </div>
                                     <div class="col-md-2 my-auto">
-                                        <label class="price">$569 </label>
+                                        <label class="price">$21000 </label>
                                     </div>
                                     <div class="col-md-2 col-7 my-auto">
                                         <div class="quantity">
@@ -62,13 +62,13 @@
                                     <div class="col-md-6 my-auto">
                                         <a href="">
                                             <label class="product-name">
-                                                <img src="hp-laptop.jpg" style="width: 50px; height: 50px" alt="">
-                                                Hp Laptop
+                                                <img src="{{}}" style="width: 50px; height: 50px" alt="">
+                                                Dress
                                             </label>
                                         </a>
                                     </div>
                                     <div class="col-md-2 my-auto">
-                                        <label class="price">$569 </label>
+                                        <label class="price">$500 </label>
                                     </div>
                                     <div class="col-md-2 col-7 my-auto">
                                         <div class="quantity">
@@ -88,34 +88,8 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="cart-item">
-                                <div class="row">
-                                    <div class="col-md-6 my-auto">
-                                        <a href="">
-                                            <label class="product-name">
-                                                <img src="hp-laptop.jpg" style="width: 50px; height: 50px" alt="">
-                                                Hp Laptop
-                                            </label>
-                                        </a>
-                                    </div>
-                                    <div class="col-md-2 my-auto">
-                                        <label class="price">$569 </label>
-                                    </div>
-                                    <div class="col-md-2 col-7 my-auto">
-                                        <div class="quantity">
-                                            <div class="input-group">
-                                                <span class="btn btn1"><i class="fa fa-minus"></i></span>
-                                                <input type="text" value="1" class="input-quantity" />
-                                                <span class="btn btn1"><i class="fa fa-plus"></i></span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-2 col-5 my-auto">
-                                        <div class="remove">
-                                            <a href="" class="btn btn-danger btn-sm">
-                                                <i class="fa fa-trash"></i> Remove
-                                            </a>
-                                        </div>
+                            
+                        
                                     </div>
                                 </div>
                             </div>
